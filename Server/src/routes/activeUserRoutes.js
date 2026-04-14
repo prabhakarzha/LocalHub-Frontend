57f2e18ae160495ef609ff736719543aa49a0290
prabhakarzha <kumarprabhakar141@gmail.com>
2026-04-14 23:24:46 +0530

    remove: server folder from frontend repo

--- Server/src/routes/activeUserRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUserCount } from "../controllers/userController.js"; // or wherever you defined it
-
-const router = express.Router();
-
-router.get("/count", getUserCount);
-
-export default router;
